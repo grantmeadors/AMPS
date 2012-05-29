f6162bd9e46109279cbbd4021befa11dd173526c
Grant Meadors <grantmeadors>
2012-05-29 09:57:12 -0700

    Have made capable of loading injections.

--- correlateInjection.m
+++ correlateInjection.m
@@ -20,6 +20,7 @@ function metadata = frameMetadata(frame)
     % The following is only compatible for nine-digit GPS times
     % gpsStart = str2num(metadata.frameString(18:26)); 
     % The following is more broadly compatible
+    metadata.injFileName = 'inj_931130713_LHO_strain.txt';
     metadata.gpsStart = str2num(timeParser(metadata.frameString));
     metadata.site = metadata.frameString(1);
     metadata.siteFull = strcat('L', metadata.site, 'O');
@@ -48,7 +49,7 @@ function metadata = frameMetadata(frame)
     % Construct a time index:
     metadata.t = metadata.gpsStart + (0:(128*metadata.fs-1))/metadata.fs;
     % Construct a filter:
-    [metadata.zb, metadata.pb, metadata.kb] = butter(16, 2*pi*[145 155], 's');
+    [metadata.zb, metadata.pb, metadata.kb] = butter(16, 2*pi*[100 110], 's');
 
     % Initially, look at reference data, 0.
     metadata.refOrFilterFlag = 0;
@@ -72,9 +73,12 @@ function dataOut = firstFrame(metadata);
 end
 
 function plots = plotMaker(metadata)
-    dataOut = firstFrame(metadata);
-    disp('Total data obtained:')
-    disp(length(dataOut))
+    if (metadata.refOrFilterFlag == 0) | (metadata.refOrFilterFlag == 1)
+        dataOut = firstFrame(metadata);
+        disp('Total data obtained:')
+        disp(length(dataOut))
+        plots.dataLength = length(dataOut);
+    end
     % Filter data:
     if metadata.refOrFilterFlag == 0
         plots.darmRef = filterZPKs(...
@@ -82,14 +86,32 @@ function plots = plotMaker(metadata)
     elseif metadata.refOrFilterFlag == 1
         plots.darmFilter = filterZPKs(...
             metadata.zb, metadata.pb, metadata.kb, metadata.fs, dataOut);
+    elseif metadata.refOrFilterFlag == 2
+        plots.strain = filterZPKs(...
+            metadata.zb, metadata.pb, metadata.kb, metadata.fs, metadata.strain);
     end
+end
+
+function strain = injectionFile(plots, metadata)
+    strain = zeros(plots.dataLength, 1);
+    strainInj = load(metadata.injFileName);
+    metadata.injStartGPS = str2num(metadata.injFileName(5:13));
+    diffGPS = metadata.injStartGPS - metadata.gpsStart;
+    diffSamp = metadata.fs * diffGPS;
+    % Presumably, the GWF files are indexed from zero, as are injections.
+    strain(diffSamp+1:diffSamp+length(strainInj)) = strainInj;
 end
 
 function plots = plotCompare(metadata)
     plots = plotMaker(metadata);
     metadata.refOrFilterFlag = 1;
     plotting = plotMaker(metadata);
+    metadata.refOrFilterFlag = 2;
     plots.darmFilter = plotting.darmFilter;
+    strain = injectionFile(plots, metadata);
+    metadata.strain = strain; 
+    plotting = plotMaker(metadata);
+    plots.strain = plotting.strain;
 end
 
 function graphing = grapher(plots, metadata)
@@ -99,20 +121,22 @@ function graphing = grapher(plots, metadata)
     outputFileHead = strcat('/home/gmeadors/public_html/feedforward/programs/spectralScan/',...
         'L', metadata.site, 'O', '/',  num2str(floor(metadata.gpsStart/1e5)), '/');
     system(horzcat('mkdir -p ', outputFileHead))
-    xlimits = metadata.gpsStart + [0 128];
+    % xlimits starting at 89 is appropriate for the injection at 931130713.
+    xlimits = metadata.gpsStart + [90.375 90.5];
     outputFile = strcat(outputFileHead, 'correlateInjection-', num2str(xlimits(1)));
-    plot(metadata.t, plots.darmRef, metadata.t, plots.darmFilter)
+    plot(metadata.t, plots.darmRef, metadata.t, plots.darmFilter, metadata.t, plots.strain)
     xlimitsIndex = metadata.fs*xlimits;
     xlim(xlimits)
     %ystdLimit = 5*std(plots.darmRef(xlimitsIndex(1):xlimitsIndex(end)));
     %ymean = mean(plots.darmRef(xlimitsIndex(1):xlimitsIndex(end)));
     %ylimits = [(ymean-ystdLimit) (ymean+ystdLimit)];
     %ylim(ylimits)
+    ylim([-3e-21 3e-21])
     %ylim([-2e-22 2e-22])
     grid on
     xlabel('Time (s)')
     ylabel('Amplitude (strain)')
-    legend('Before feedforward', 'After feedforward')
+    legend('Before feedforward', 'After feedforward', 'Injection estimated strain')
     titleString = horzcat('Post-filtering injection, GPS s ', num2str(xlimits(1)), ' to ', num2str(xlimits(end)))
     title(titleString)
     disp(outputFile)
